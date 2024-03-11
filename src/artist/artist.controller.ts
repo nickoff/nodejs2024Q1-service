7584459ddaa971c73993fc4fb7600c9c669352a6
nickoff <nick.olehnovich@gmail.com>
2024-03-11 21:47:55 +0300

    feat: add description OpenAPI Artist instance

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -11,7 +11,13 @@ import {
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { Artist } from './entities/artist.entity';
 
 @ApiTags('Artists')
 @Controller('artist')
@@ -19,27 +25,117 @@ export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create an artist.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Request is valid.',
+    type: Artist,
+    schema: {
+      $ref: getSchemaPath(Artist),
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Bad Request error. Request body does not contain required fields.',
+  })
   create(@Body() createArtistDto: CreateArtistDto) {
     return this.artistService.create(createArtistDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all artists records',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Request is valid.',
+    type: [Artist],
+    schema: {
+      $ref: getSchemaPath(Artist),
+    },
+  })
   findAll() {
     return this.artistService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get the artist by id',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Request is valid.',
+    type: Artist,
+    schema: {
+      $ref: getSchemaPath(Artist),
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Bad Request error. Request body does not contain required fields.',
+  })
+  @ApiResponse({
+    status: 404,
+    description:
+      "Not Found error. Record with 'id === artistId' doesn't exist.",
+  })
   findOne(@Param('id') id: string) {
     return this.artistService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({
+    summary: 'Update the artist',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Request is valid.',
+    type: Artist,
+    schema: {
+      $ref: getSchemaPath(Artist),
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Bad Request error. Request body does not contain required fields.',
+  })
+  @ApiResponse({
+    status: 404,
+    description:
+      "Not Found error. Record with 'id === artistId' doesn't exist.",
+  })
   update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
     return this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
+  @ApiOperation({
+    summary: 'Delete the artist by id',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'Request is valid. Record is deleted.',
+    type: Boolean,
+    schema: {
+      $ref: getSchemaPath('true'),
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Bad Request error. Request body does not contain required fields.',
+  })
+  @ApiResponse({
+    status: 404,
+    description:
+      "Not Found error. Record with 'id === artistId' doesn't exist.",
+  })
   remove(@Param('id') id: string) {
     return this.artistService.remove(id);
   }
--- src/artist/entities/artist.entity.ts
+++ src/artist/entities/artist.entity.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Artist {
+  @ApiProperty()
   id: string;
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   grammy: boolean;
 }
